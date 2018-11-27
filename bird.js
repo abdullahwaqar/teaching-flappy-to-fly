ecdd9ca4d32d59419641df75d0ef631099d0a634
awr417h <abdullahwaqar8@gmail.com>
2018-11-28 01:55:01 +0500

    Implemented the final fixes

--- bird.js
+++ bird.js
@@ -1,85 +1,107 @@
-class Bird {
+// Mutation function to be passed into bird.brain
+function mutate(x) {
+    if (random(1) < 0.1) {
+      let offset = randomGaussian() * 0.5;
+      let newx = x + offset;
+      return newx;
+    } else {
+      return x;
+    }
+  }
+
+  class Bird {
     constructor(brain) {
-        this.y = height / 2;
-        this.x = 64;
-        this.radius = 12;
-        this.gravity = 0.6;
-        this.velocity = 0;
-        this.liftForce = -12;
+      // position and size of bird
+      this.x = 64;
+      this.y = height / 2;
+      this.r = 12;
 
-        this.score = 0;
-        this.fitness = 0;
-        /*
-        ? Declaring the Brain of the bird/ball
-        ?@param: input[4] hiddenNode[4] output[2]
-        */
-       if (brain) {
-           this.brain = brain.copy();
-       } else {
-           this.brain = new NeuralNetwork(4, 4, 12);
-       }
+      // Gravity, lift and velocity
+      this.gravity = 0.8;
+      this.lift = -12;
+      this.velocity = 0;
+
+      // Is this a copy of another Bird or a new one?
+      // The Neural Network is the bird's "brain"
+      if (brain instanceof NeuralNetwork) {
+        this.brain = brain.copy();
+        this.brain.mutate(mutate);
+      } else {
+        this.brain = new NeuralNetwork(5, 8, 2);
+      }
+
+      // Score is how many frames it's been alive
+      this.score = 0;
+      // Fitness is normalized version of score
+      this.fitness = 0;
     }
 
+    // Create a copy of this bird
+    copy() {
+      return new Bird(this.brain);
+    }
+
+    // Display the bird
     show() {
-        stroke(255);
-        fill(255, 50);
-        ellipse(this.x, this.y, this.radius * 2, this.radius * 2);
+      fill(255, 100);
+      stroke(255);
+      ellipse(this.x, this.y, this.r * 2, this.r * 2);
     }
 
-    liftUp() {
-        this.velocity += this.liftForce;
-    }
-
-    mutate() {
-        this.brain.mutate(0.1);
-    }
-
+    // This is the key function now that decides
+    // if it should jump or not jump!
     think(pipes) {
-        //* Find the closet pipe
-        let closetPipe = null;
-        let closetDistance = Infinity;
-        for (let i = 0; i < pipes.length; i++) {
-            let d = pipes[i].x - this.x;
-            if (d < closetDistance) {
-                closetPipe = pipes[i];
-                closetDistance = d;
-            }
+      // First find the closest pipe
+      let closest = null;
+      let record = Infinity;
+      for (let i = 0; i < pipes.length; i++) {
+        let diff = pipes[i].x - this.x;
+        if (diff > 0 && diff < record) {
+          record = diff;
+          closest = pipes[i];
         }
+      }
 
-        let inputs =[];
-        inputs[0] = this.y / height;
-        inputs[1] = closetPipe.top / height;
-        inputs[2] = closetPipe.bottom / height;
-        inputs[3] = closetPipe.x / width;
+      if (closest != null) {
+        // Now create the inputs to the neural network
+        let inputs = [];
+        // x position of closest pipe
+        inputs[0] = map(closest.x, this.x, width, 0, 1);
+        // top of closest pipe opening
+        inputs[1] = map(closest.top, 0, height, 0, 1);
+        // bottom of closest pipe opening
+        inputs[2] = map(closest.bottom, 0, height, 0, 1);
+        // bird's y position
+        inputs[3] = map(this.y, 0, height, 0, 1);
+        // bird's y velocity
+        inputs[4] = map(this.velocity, -5, 5, 0, 1);
 
-        //// let input = [1.0, 0.5, 0.2, 0.3];
-        let output = this.brain.predict(inputs);
-        if (output[0] > output[1]) {
-            this.liftUp();
+        // Get the outputs from the network
+        let action = this.brain.predict(inputs);
+        // Decide to jump or not!
+        if (action[1] > action[0]) {
+          this.up();
         }
+      }
     }
 
+    // Jump up
+    up() {
+      this.velocity += this.lift;
+    }
+
+    bottomTop() {
+      // Bird dies when hits bottom?
+      return (this.y > height || this.y < 0);
+    }
+
+    // Update bird's position based on velocity, gravity, etc.
     update() {
-        this.score++;
-        this.velocity += this.gravity;
-        this.velocity *= 0.9; // air resistance
-        this.y += this.velocity;
+      this.velocity += this.gravity;
+      // this.velocity *= 0.9;
+      this.y += this.velocity;
 
-        /*
-        * If the ball hits the ground stop it falling
-        * & reset the velocity back to 0.
-        */
-        if (this.y > height) {
-            this.y = height;
-            this.velocity = 0;
-        }
-        /*
-        * If the ball hits the top of the window stop the ball
-        * & reset the velocity back to 0.
-        */
-        if (this.y < 0) {
-            this.y = 0;
-            this.velocity = 0;
-        }
+      // Every frame it is alive increases the score
+      this.score++;
     }
-}
+  }
